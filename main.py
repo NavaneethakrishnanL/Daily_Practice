c280c68c0b9a1b5fc914bcff0887d12facd6bb37
NavaneetrhakrishnanL <nkrishnan.laksh@gmail.com>
2023-05-23 22:02:58 +0530

    Navaneeth Add orange to cart

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from product import Product
 from cart import Cart
 product1=Product("Apple", 20, 200)
+product2=Product("Oranges", 30, 300)
 cart1=Cart()
 cart1.add_to_cart(product1)
+cart1.add_to_cart(product2)
 # product1.show()
 cart1.show_cart()
